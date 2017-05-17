3ebb7854cfaa5185e42b13835cad2e1e163a2bc7
Thomas Malthouse <tmalthouse@gmail.com>
2017-05-17 11:48:09 -0700

    Initial Commit

---
+++ nbody2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  nbody2
+//
+//  Created by Thomas Malthouse on 5/17/17.
+//  Copyright © 2017 Thomas Malthouse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
